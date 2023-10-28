be2a597760d170b7489214889860088ef5761582
Kev Koppa <koppakev@gmail.com>
2023-10-28 16:16:32 -0500

    added firebase

--- pinpoint/src/index.js
+++ pinpoint/src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnvujRiXxhZxEraljFTXrEwKKY5uialPY",
+  authDomain: "pinpoint-477c9.firebaseapp.com",
+  projectId: "pinpoint-477c9",
+  storageBucket: "pinpoint-477c9.appspot.com",
+  messagingSenderId: "744530718614",
+  appId: "1:744530718614:web:b838de0f3aae8e17ca37b7",
+  measurementId: "G-6MRJ1JTB2D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
